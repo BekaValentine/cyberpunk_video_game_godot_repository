f288e8a005ac22f9c88437e1350e9c486f0f7eb4
Beka Valentine <beka@gothdyke.mom>
2024-01-03 13:09:36 -0800

    Abstracts unlock logic

--- infrastructure/objects/Lock.gd
+++ infrastructure/objects/Lock.gd
@@ -14,6 +14,9 @@ func _ready():
 	add_child(timer)
 	timer.connect("timeout", self, "unlock")
 	
+func should_unlock(key):
+	return key.key_type and key.key_type in self.lock_types
+
 func unlock():
 	locked = false
 	debug_info.log("lock state changed" + str(self), self.locked)
@@ -22,14 +25,12 @@ func highlight_ended():
 	timer.stop()
 
 func use(agent, tool_object):
-	if locked and "skills" in agent and tool_object is Key:
-		
+	if locked and "skills" in agent and tool_object is Key and self.should_unlock(tool_object):
 		var player_skill = agent.skills["lock"]
-		if tool_object.key_type and tool_object.key_type in self.lock_types:
-			var actual_unlock_time = self.max_unlock_time * (1 - player_skill) * (1 - tool_object.attack_strength)
-			debug_info.log("unlock time", actual_unlock_time)
-			if actual_unlock_time == 0:
-				self.unlock()
-			else:
-				timer.wait_time = actual_unlock_time
-				timer.start()
+		var actual_unlock_time = self.max_unlock_time * (1 - player_skill) * (1 - tool_object.attack_strength)
+		debug_info.log("unlock time", actual_unlock_time)
+		if actual_unlock_time == 0:
+			self.unlock()
+		else:
+			timer.wait_time = actual_unlock_time
+			timer.start()
