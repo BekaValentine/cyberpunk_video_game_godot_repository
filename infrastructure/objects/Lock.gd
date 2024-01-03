72620bf921c164fa7a5e2f5f854795a0376844e9
Beka Valentine <beka@gothdyke.mom>
2024-01-03 14:22:30 -0800

    Adds boltcutters and lock destruction

--- infrastructure/objects/Lock.gd
+++ infrastructure/objects/Lock.gd
@@ -5,6 +5,7 @@ var lock_types = []
 var max_unlock_time = 0.0
 var timer = null
 var locked = true
+var cut_durability = 5
 
 func _init():
 	interactable = true
@@ -25,12 +26,16 @@ func highlight_ended():
 	timer.stop()
 
 func use(agent, tool_object):
-	if locked and "skills" in agent and tool_object is Key and self.should_unlock(tool_object):
-		var player_skill = agent.skills["lock"]
-		var actual_unlock_time = self.max_unlock_time * (1 - player_skill) * (1 - tool_object.attack_strength)
-		debug_info.log("unlock time", actual_unlock_time)
-		if actual_unlock_time == 0:
-			self.unlock()
-		else:
-			timer.wait_time = actual_unlock_time
+	if locked:
+		if "skills" in agent and tool_object is Key and self.should_unlock(tool_object):
+			var player_skill = agent.skills["lock"]
+			var actual_unlock_time = self.max_unlock_time * (1 - player_skill) * (1 - tool_object.attack_strength)
+			debug_info.log("unlock time", actual_unlock_time)
+			if actual_unlock_time == 0:
+				self.unlock()
+			else:
+				timer.wait_time = actual_unlock_time
+				timer.start()
+		elif tool_object is BoltCutters:
+			timer.wait_time = self.cut_durability
 			timer.start()
