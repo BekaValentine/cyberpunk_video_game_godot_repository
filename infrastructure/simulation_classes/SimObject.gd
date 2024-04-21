0140a5867b016f440cd12ca0fa01f0ee058306d1
Rebecca Valentine <171941+BekaValentine@users.noreply.github.com>
2024-04-20 19:23:44 -0700

    Revert "Adds new changes to focus"

--- infrastructure/simulation_classes/SimObject.gd
+++ infrastructure/simulation_classes/SimObject.gd
@@ -16,8 +16,7 @@ var interactable = false
 var meshes = []
 var focal_object_resource = null
 var focal_object = null
-var in_focus_object_resource = null
-var in_focus_object = null
+var root_object = null
 
 func _ready():
 	add_visual_instances(self)
@@ -32,6 +31,9 @@ func add_visual_instances(node):
 func move_meshes_to_layers(layers):
 	for m in meshes:
 		m.layers = layers
+
+func set_root_object(obj):
+	self.root_object = obj
 
 func can_highlight():
 	return self.get_holdable() or interactable or focal_object_resource != null
@@ -49,9 +51,6 @@ func _hold():
 	if not self.holdable: return
 
 	self.move_meshes_to_layers(2)
-
-	if self.in_focus_object:
-		self.in_focus_object.get_parent().remove(self.in_focus_object)
 	
 	self.hold()
 
@@ -84,6 +83,7 @@ func _focus():
 	debug_info.log("focused on an object", self)
 	self.focus()
 	focal_object = focal_object_resource.instance()
+	focal_object.set_root_object(self)
 	return focal_object
 
 func _unfocus():
